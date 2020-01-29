61744bc607277fe91be634bb486cc8b5ea1fe3e4
HuangMinShi <z7707092003@gmail.com>
2020-01-29 21:20:55 +0800

    Add counties json

--- controllers/cartController.js
+++ controllers/cartController.js
@@ -19,7 +19,7 @@ const shippingMethods = {
   directDelivery: formatNumberToCurrency(100)
 }
 
-const countyList = ['臺北市', '新北市', '桃園市', '臺中市', '臺南市', '高雄市', '新竹縣', '苗栗縣', '彰化縣', '南投縣', '雲林縣', '嘉義縣', '屏東縣', '宜蘭縣', '花蓮縣', '臺東縣', '基隆市', '新竹市', '嘉義市', '澎湖縣', '金門縣', '連江縣']
+const countyList = require('../public/counties.json').counties
 
 const cartController = {
   getCart: async (req, res) => {
