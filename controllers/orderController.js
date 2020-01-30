94f3a8f05d56aca06f74900fdc3f88e234744207
HuangMinShi <z7707092003@gmail.com>
2020-01-30 14:45:24 +0800

    Add display create page data

--- controllers/orderController.js
+++ controllers/orderController.js
@@ -1,10 +1,15 @@
 const {
-  Order
+  Cart,
+  Order,
+  Color,
+  Image,
+  Product
 } = require('../models')
 
 const {
   formatDateToYYYYMMDD,
   formatNumberToCurrency,
+  formatCurrencyToNumber,
   mapOrderStatusCodeToString
 } = require('../libs/utils')
 
@@ -50,10 +55,81 @@ const orderController = {
     return res.send('get order')
   },
 
-  createOrder: (req, res) => {
+  createOrder: async (req, res) => {
+    req.session.cartId = 1
     const cartInfo = req.query
+    const options = {
+      name: '蔡易軒',
+      township: '南投市',
+      street: '埔里鎮民生路127號',
+      tel: '0912387122'
+    }
 
-    return res.render('create', { counties, shippingMethods })
+    // 找出購物車商品
+    let cart = await Cart.findByPk(req.session.cartId, { include: 'cartItems' })
+
+    // 資料整理
+    let cartItems = cart.cartItems.map(item => ({
+      size: item.size,
+      ProductId: item.ProductId,
+      CartProductSkuId: item.CartProductSku.id,
+      quantity: item.CartProductSku.quantity,
+    }))
+
+    // 找出商品資訊
+    const products = await Product.findAll({
+      attributes: ['id', 'sn', 'name', 'salePrice'],
+      where: {
+        id: cartItems.map(item => item.ProductId)
+      },
+      include: [
+        {
+          model: Color,
+          attributes: ['type']
+        },
+        {
+          model: Image,
+          attributes: ['url'],
+          where: { isMain: true }
+        }
+      ]
+    })
+
+    // 購物車商品與商品資訊合併
+    cartItems = cartItems.map(item => {
+      const product = products.find(product => product.id === item.ProductId)
+
+      return {
+        ...item,
+        sn: product.sn,
+        name: product.name,
+        image: product.Images[0].url,
+        color: product.Color.type.split('_').join(' ').toUpperCase(),
+        salePrice: formatNumberToCurrency(Number(product.salePrice)),
+        itemTotal: formatNumberToCurrency(Number(product.salePrice) * Number(item.quantity))
+      }
+    })
+
+    // 運費及價格
+    const selectedShippingWay = shippingMethods[cartInfo.shipping] || []
+    const len = selectedShippingWay.length
+    const selectedShippingFee = selectedShippingWay[len - 1] || ''
+
+    let subTotal = cartItems.length ? cartItems.map(item => formatCurrencyToNumber(item.itemTotal)).reduce((a, c) => a + c) : 0
+    let total = subTotal + formatCurrencyToNumber(selectedShippingFee)
+    subTotal = formatNumberToCurrency(subTotal)
+    total = formatNumberToCurrency(total)
+
+    return res.render('create', {
+      ...options,
+      ...cartInfo,
+      cartItems,
+      counties,
+      shippingMethods,
+      selectedShippingFee,
+      subTotal,
+      total
+    })
   },
 
   postOrder: (req, res) => {
