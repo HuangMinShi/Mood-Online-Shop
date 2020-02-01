b1b759d9fc38b3b6d00c612edde33153fdf020f5
HuangMinShi <z7707092003@gmail.com>
2020-02-01 17:31:39 +0800

    Add getShippingFee func

--- libs/utils.js
+++ libs/utils.js
@@ -1,5 +1,7 @@
 const moment = require('moment')
 
+const { shippingMethods } = require('../public/shippingMethods.json')
+
 const utils = {
   formatDateToYYYYMMDD: (date) => {
     return moment(date).format('YYYY/MM/DD')
@@ -14,6 +16,12 @@ const utils = {
       '3': '完成'
     }
     return orderStatusCodesList[statusNum.toString()]
+  },
+
+  getShippingFee: (shippingWay) => {
+    const content = shippingMethods[shippingWay] || []
+    const fee = Number(content[content.length - 1])
+    return fee || 0
   }
 }
 
