551fe3fc2ea161444003c4c5e76ce596dd747b9a
HuangMinShi <z7707092003@gmail.com>
2020-02-05 16:56:49 +0800

    Add handlebars helpers

--- libs/handlebarsHelpers.js
+++ libs/handlebarsHelpers.js
@@ -8,12 +8,22 @@ module.exports = {
   currency: (price) => {
     const priceToNum = Number(price)
 
-    if (price === '' || isNaN(priceToNum)) {
-      return price
-    }
+    if (price === '' || isNaN(priceToNum)) return price
 
     const options = { style: 'currency', currency: 'TWD' }
     const currency = priceToNum.toLocaleString('zh-TW', options)
     return currency.split('.', 1)[0]
-  }
+  },
+
+  shippingWayToMandarin: (way) => {
+    return shippingMethods[way][0]
+  },
+
+  receiptToMandarin: (index) => {
+    return receiptMethods[index]
+  },
+
+  paymentToMandarin: (index) => {
+    return paymentMethods[index]
+  },
 }
