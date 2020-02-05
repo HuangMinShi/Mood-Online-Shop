8ad9480cd4e3b26e0d1b9e562a3235c2cb133427
HuangMinShi <z7707092003@gmail.com>
2020-02-05 16:54:27 +0800

    Add order routes

--- routes/orders.js
+++ routes/orders.js
@@ -4,17 +4,25 @@ const router = express.Router()
 const { vaildateInput } = require('../middlewares/validator')
 const orderController = require('../controllers/orderController')
 
+// 瀏覽所有訂單
 router.get('/', orderController.getOrders)
+
+// 配送資訊
 router.get('/checkout/shipping', orderController.getCheckoutShipping)
+router.post('/checkout/shipping', vaildateInput, orderController.postCheckoutShipping)
 
-router.post('/checkout/shipping',
-  vaildateInput,
-  orderController.postCheckoutShipping
-)
-
+// 付款資訊
 router.get('/checkout/payment', orderController.getCheckoutPayment)
+router.post('/checkout/payment', vaildateInput, orderController.postCheckoutPayment)
 
+// 訂單確認
+router.get('/checkout/order', orderController.getCheckoutOrder)
 router.post('/', orderController.postOrder)
+
+// 訂單成立
+router.get('/success', orderController.getSuccessOrder)
+
+// 單一訂單操作
 router.get('/:id', orderController.getOrder)
 router.post('/:id/cancel', orderController.cancelOrder)
 router.get('/:id/payment', orderController.getPayment)
