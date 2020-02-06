13e61c5e3bbb4b79a5ced7bb90112c8f09f9d7e0
HuangMinShi <z7707092003@gmail.com>
2020-02-06 09:36:04 +0800

    Add POST order feature

--- controllers/orderController.js
+++ controllers/orderController.js
@@ -1,3 +1,5 @@
+const bcrypt = require('bcryptjs')
+
 const {
   User,
   Order,
@@ -15,6 +17,7 @@ const {
 const { counties } = require('../config/geonames.json')
 const { shippingMethods } = require('../config/business.json')
 const { shippingOptions } = require('../config/options.json')
+const { createTransport, generateMail } = require('../libs/mail')
 
 const orderController = {
   getOrders: async (req, res) => {
@@ -103,10 +106,117 @@ const orderController = {
 
     return res.render('checkoutOrder', orderInfo)
   },
+
+  postOrder: async (req, res) => {
+    try {
+      const orderInfo = req.flash('data')[0]
+      // 若 err 發生，則返回上一頁能續帶上次資料
+      req.flash('data', orderInfo)
+
+      // 新增 user
+      const salt = await bcrypt.genSalt(10)
+      const passwordInHash = await bcrypt.hash('HelloKitty24', salt)
+      const [user, isCreated] = await User.findOrCreate({
+        where: {
+          email: orderInfo.email
+        },
+        defaults: {
+          email: orderInfo.email,
+          password: passwordInHash,
+          name: orderInfo.receiveName,
+          address: orderInfo.receiveAddress,
+          tel: orderInfo.receivePhone,
+          role: 'visitor'
+        }
+      })
+
+      if (isCreated) {
+        // 寄信通知未完成註冊
+      }
+
+      // 避免併發狀況，再確認庫存
+      const skus = orderInfo.cartItems.map(item => item.sku)
+      let productSkus = await ProductSku.findAll({
+        attributes: ['id', 'sku', 'stock'],
+        where: {
+          sku: skus
+        }
+      })
+
+      productSkus = productSkus
+        .map(item => {
+          const sku = item.sku
+          delete item.dataValues.sku
+          return {
+            [sku]: item.dataValues
+          }
+        })
+        .reduce((prev, curr) => Object.assign(prev, curr))
+
+      const errors = []
+      orderInfo.cartItems.forEach(item => {
+        const stock = productSkus[item.sku].stock
+        if (stock < item.quantity) {
+          errors.push({
+            message: `哎呀！晚了一步，商品 ${item.name}，庫存數量為 ${item.stock}，庫存不足請確認`
+          })
+        }
+      })
+
+      if (errors.length) {
+        req.flash('errors', errors)
+        return res.redirect('/cart')
+      }
+
+      // 新增 order
+      const order = await Order.create({
+        ...orderInfo,
+        UserId: user.id
+      })
+
+      const sn = '000000' + order.id
+      await order.update({ sn })
+
+      // 新增 orderItems
+      const orderItems = orderInfo.cartItems.map(item => {
+        return OrderProductSku.create({
+          quantity: item.quantity,
+          OrderId: order.id,
+          ProductSkuId: productSkus[item.sku].id
+        })
+      })
+
+      await Promise.all(orderItems)
+
+      // 清除購物車及商品資料
+      // await Cart.destroy({
+      //   where: {
+      //     id: req.session.cartId
+      //   }
+      // })
+
+      // 寄發訂單成立 mail
+      const mail = generateMail(orderInfo, sn)
+      const transporter = createTransport()
+      transporter.sendMail(mail, (err, info) => {
+        if (err) return console.log(err);
+        return console.log(`Email sent：${info.response}`);
+      })
+
+      // 若成功額外帶往下一個頁面
+      req.flash('order', order)
+
+      return res.redirect('/orders/success')
+
+    } catch (err) {
+      if (err.name === 'SequelizeValidationError') {
+        req.flash('errorMessage', err.errors[0].message)
+        return res.redirect('back')
+      }
+      return console.log(err);
+    }
   },
 
-  postOrder: (req, res) => {
-    return res.send('post order')
   },
 
   cancelOrder: (req, res) => {
