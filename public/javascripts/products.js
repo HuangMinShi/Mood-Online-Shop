1d61b9ae3e23e485167b3ca8f7e135fde5adf1ca
HuangMinShi <z7707092003@gmail.com>
2020-02-16 17:30:39 +0800

    Add products UI style

--- public/javascripts/products.js
+++ public/javascripts/products.js
@@ -1,17 +1,23 @@
 import { switchProductMainImage } from './libs.js'
 
 (function () {
+  // 預設
   $('[class=product-img] img:first-child').each(function () {
     $(this).addClass('selected')
   })
 
   $('.products').click(function (event) {
 
-    if (event.target.matches('.color-option')) {
+    if (event.target.matches('.color')) {
+      const target = event.target
       const productSn = event.target.dataset.productSn
       const colorOption = event.target.dataset.colorOption
 
       switchProductMainImage(productSn, colorOption)
+
+      // 選取顏色時變化
+      $('.color-img-wrapper.selected').removeClass('selected')
+      $(target).parent().addClass('selected')
     }
 
   })
